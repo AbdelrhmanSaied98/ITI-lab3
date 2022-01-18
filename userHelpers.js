f4afed0118c5df49883e51dedf2180018874f7ba
AbdelrhmanSaied98 <abdelrahmansaied992@gmail.com>
2022-01-18 16:55:00 +0200

    Complete validateUser function

--- userHelpers.js
+++ userHelpers.js
@@ -4,6 +4,8 @@ const fs = require('fs')
 const validateUser = async (req, res, next) =>{
     try {
         const {username , password} = req.body;
+        if(!username) return next({status:422, message:"username is requird"})
+        if(!password) return next({status:422, message:"password is requird"})
         const data = await fs.promises.readFile('./user.json',{encoding:'utf8'})
         const users = JSON.parse(data)
         const isUsernameExists = users.some(user=>user.username===username)
